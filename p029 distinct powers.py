1417e20810c9c76db1efb4d982bb6b98da39fb8e
Markus Engsner <markus.engsner.other@gmail.com>
2019-03-03 15:13:55 +0100

    modulo check & profiling

--- p029 distinct powers.py
+++ p029 distinct powers.py
@@ -1,23 +1,21 @@
 from time import time
+import cProfile
 
 def isPower(n, base):
-    exponent = 1
-    result = base ** exponent
-    while result < n:
-        result = base ** exponent
-        if result == n:
-            return True
-        exponent += 1
-    return False
+    result = n / base
+    while result > 1:
+        result /= base
+    return result == 1
 
 
 def powerOf(n, baseDict):
     for base in baseDict:
-        if base ** 2 > n:
-            return 0
-        if isPower(n, base):
-            return base
-    return 0
+        if base ** 2 > n: # purely for performance
+            return n
+        if n % base == 0: # purely for performance
+            if isPower(n, base):
+                return base
+    return n
 
 
 def buildBaseDict(lower, higher):
@@ -25,25 +23,21 @@ def buildBaseDict(lower, higher):
     baseDict = {}
     for i in range(lower, higher + 1):
         base = powerOf(i, baseDict)
-        if base == 0:
-            baseDict[i] = 1
-        else:
-            baseDict[base] += 1
+        baseDict[base] = baseDict.get(base, 0) + 1
     return baseDict
 
 def buildNewUniques(n, minExp, maxExp):
     ''' builds a dict with exponents as keys 
         and amount of new, unique, powers as values
         n: greatest multiple '''
-    base = 2 # could be any base
     uniqueDict = {i:0 for i in range(1, n + 1)}
-    powerSet = set()
+    expSet = set()
     for i in range(1, n + 1):
         for j in range(minExp, maxExp +1):
-            power = base ** (j * i)
-            if not power in powerSet:
+            exponent = j * i
+            if not exponent in expSet:
                 uniqueDict[i] += 1
-                powerSet.add(power)
+                expSet.add(exponent)
     compoundedDict = {1:uniqueDict[1]}
     for i in range(2, n+1):
         compoundedDict[i] = uniqueDict[i] + compoundedDict[i-1]
@@ -62,16 +56,20 @@ def compute(limit):
     seen = set(a**b for a in range(2, limit + 1) for b in range(2, limit + 1))
     return str(len(seen))
 
-start = time()
-low = 2
-high = 2000
-answer = uniqueCount(low, high)
-end = time()
-print("Answer: {}".format(answer))
-print("Took {} ms".format(round(1000*(end - start))))
+def runCode():
+    # start = time()
+    low = 2
+    high = 100
+    answer = uniqueCount(low, high)
+    # end = time()
+    print("Limit: {}".format(high))
+    print("Answer: {}".format(answer))
+    print("Took {} ms".format(round(10 ** 3 * (end - start))))
+    #Takes about 0.29 ms on my machine (tested using timeit)
+    """ answer = compute(high)
+    alt = time()
+    print("Answer: {}".format(answer))
+    print("Took {} ms".format(round(10 ** 3 * (alt - end)))) """
 
-answer = compute(high)
-alt = time()
-print("Answer: {}".format(answer))
-print("Took {} ms".format(round(1000*(alt - end))))
-# Takes about 4 ms on my machine
+#cProfile.run('runCode()')
+runCode()
